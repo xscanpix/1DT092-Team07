7f143e557a1399ccd1a778faf913e5bcfbef6b80
Svante Nilsson <svante.n@live.se>
2018-05-10 15:03:22 +0200

    Can encode and decode simple messages

--- Server/src/robot/RobotMessage.java
+++ Server/src/robot/RobotMessage.java
@@ -1,45 +1,93 @@
 package robot;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
 public class RobotMessage {
+
+    private static int OPCODE_BYTES = 2;
 
     // OP|DATA
     private enum OPS {
         OP_1, OP_2
     }
 
-    private byte[] byteMessage;
+    private short op;
+    private String data;
 
-    private Integer op;
-
-    public RobotMessage(byte[] byteMessage) {
-        if (byteMessage != null) {
-            this.byteMessage = byteMessage;
-        } else {
-            throw new NullPointerException();
-        }
-        this.op = null;
+    private RobotMessage(short op, String data) {
+        this.op = op;
+        this.data = data;
     }
 
-    private int getOp() {
+    private short getOp() {
         return op;
     }
 
-    public RobotMessage parseMessage() throws RobotMessageException {
+    private String getData() {
+        return data;
+    }
+
+    private static boolean opIsNotValid(short op) {
+        try {
+            OPS res = OPS.values()[op];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static byte[] encodeMessage(short op, String data) throws RobotMessageException {
+        if(opIsNotValid(op)) {
+            throw new RobotMessageException("Operation is not valid");
+        }
+
+        if(data == null) {
+            data = "";
+        }
+
+        int len = 0;
+
+        int opLen = OPCODE_BYTES;
+        int dataLen = data.getBytes().length;
+
+        len += (opLen + dataLen);
+
+        ByteBuffer buf = ByteBuffer.allocate(len);
+
+        buf.putShort(op);
+        buf.put(data.getBytes());
+
+        return buf.array();
+    }
+
+    public static RobotMessage decodeMessage(byte[] message) throws RobotMessageException {
+
+        ByteBuffer buf = ByteBuffer.wrap(message);
+
+        short op;
+        String data;
 
         try {
-            op = (int) (byteMessage[0]);
+            op = buf.getShort();
         } catch (ClassCastException e) {
             throw new RobotMessageException("First byte is not an integer");
         }
 
-        try {
-            OPS res = OPS.values()[op];
-        } catch (ArrayIndexOutOfBoundsException e) {
-            throw new RobotMessageException("Operation does not exist");
+        if(opIsNotValid(op)) {
+            throw new RobotMessageException("Operation is not valid");
         }
 
-        return this;
+        byte[] remaining = new byte[buf.remaining()];
+        buf.get(remaining);
+
+        data = new String(remaining, StandardCharsets.UTF_8);
+
+        return new RobotMessage(op, data);
     }
 
-
+    public String toString() {
+        return op + data;
+    }
 }
