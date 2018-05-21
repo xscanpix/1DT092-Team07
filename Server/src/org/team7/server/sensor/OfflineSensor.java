e70c5d7ecd580e5c47ea348ed5ce02ebc6403774
Svante Nilsson <svante.n@live.se>
2018-05-21 11:51:26 +0200

    Cleaned up and improved

--- Server/src/org/team7/server/sensor/OfflineSensor.java
+++ Server/src/org/team7/server/sensor/OfflineSensor.java
@@ -1,28 +1,29 @@
 package org.team7.server.sensor;
 
 import org.team7.server.sensor.sensormessage.SensorMessageReadings;
-import org.team7.server.sensor.sensormessage.SensorMessageSetup;
 
+import java.nio.ByteBuffer;
 import java.util.Random;
 
 public class OfflineSensor extends Sensor {
-    public OfflineSensor(int id, int x, int y) {
-        super();
-        this.id = id;
+    public OfflineSensor(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    @Override
+    public void sendMessage(ByteBuffer buf) {
+
+    }
+
     public void start(int msBetweenSend) {
         new Thread(() -> {
-            messages.offer(new SensorMessageSetup(id, x, y));
-
-            while (true) {
-                messages.offer(new SensorMessageReadings(getId(), new Random().nextInt(), new Random().nextInt()));
+            while(true) {
+                queueFromSensor.offer(new SensorMessageReadings(getId(), new Random().nextInt(500), new Random().nextInt(500)));
 
                 try {
                     Thread.sleep(msBetweenSend);
-                } catch (InterruptedException e) {
+                } catch(InterruptedException e) {
                     break;
                 }
             }
