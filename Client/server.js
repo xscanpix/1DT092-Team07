45cec6434e6c7e615f856cce24b07293db5e2251
quangkhovigaitheo <nhatquangprovodoi@gmail.com>
2018-05-23 23:13:13 +0700

    add client

---
+++ Client/server.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+    setInterval(function() {
+        $.ajax({
+            url: "http://127.0.0.1:8080/get/robot",
+            crossDomain: true,
+            success: function(result){
+                var data = JSON.parse(result);
+                console.log(data)
+                var temp = data.Direction;
+                var speed = data.Direction;
+
+                $('#temp_display').html(temp);
+                $('#speed_display').html(speed);
+            }
+        });
+    }, 500);
+
+});
